9b81b1f48e7e49650ceb2a2167ddc2c2bcba597e
johnmschneider <john1schneider1@gmail.com>
2020-02-24 12:04:52 -0500

    update to main index.html

---
+++ navbar.js
@@ -0,0 +1,18 @@
+function createNavButton(buttonText, link) {
+    let button = document.createElement("INPUT");
+    
+    button.type = "button";
+    button.value = buttonText;
+    button.className = "navbar";
+    button.addEventListener("click", function(){ location.href = link; });
+    
+    document.getElementById("navbar").appendChild(button);
+}
+
+function createNavbar() {
+    createNavButton("Home", "index.html");
+    createNavButton("Projects", "/projects/");
+    createNavButton("RPG game 1", "/projects/rpg game 1/rpgIndex.html");
+}
+
+createNavbar();
