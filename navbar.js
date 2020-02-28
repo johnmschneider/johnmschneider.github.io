917bd777be02516cf50a7f79bc2b0e4ddca45851
johnmschneider <john1schneider1@gmail.com>
2020-02-27 20:31:51 -0500

    added projects page

--- navbar.js
+++ navbar.js
@@ -4,14 +4,27 @@ function createNavButton(buttonText, link) {
     button.type = "button";
     button.value = buttonText;
     button.className = "navbar";
-    button.addEventListener("click", function(){ location.href = link; });
+    button.addEventListener("click", function() {
+        location.href = link;
+        button.style.fontWeight = 'bold';
+        //since we navigated to a new page we don't have to unbold the other buttons
+    });
     
-    document.getElementById("navbar").appendChild(button);
+    document.getElementById("navbarButtons").appendChild(button);
+    return button;
 }
 
 function createNavbar() {
-    createNavButton("Home", "index.html");
-    createNavButton("Projects", "/projects/");
+    var homeButton = createNavButton("Home", "index.html");
+    
+    // the main index won't be able to use the click event to load itself (browser is
+    //  directly navigating to the website)
+    if (location.href.search("https://johnmschneider.github.io/index.html") != 0) {
+        homeButton.style.fontWeight = 'bold';
+    } 
+    
+    createNavButton("Projects", "https://johnmschneider.github.io/projects/" +
+        "projectIndex.html");
     //createNavButton("RPG game 1", "/projects/rpg game 1/rpgIndex.html");
 }
 
