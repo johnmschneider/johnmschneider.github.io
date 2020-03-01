370a2416e080dc5eee800a42a1c7632405bda7d4
johnmschneider <john1schneider1@gmail.com>
2020-02-29 20:01:31 -0500

    Update settlement.js

--- projects/rpg game 1/pointsOfInterest/settlement.js
+++ projects/rpg game 1/pointsOfInterest/settlement.js
@@ -1,5 +1,5 @@
 import { PointOfInterest } from 
-    "https://johnmschneider.github.io/projects/rpg%20game%201/pointOfInterest.js";
+    "https://johnmschneider.github.io/projects/rpg%20game%201/pointsOfInterest/pointOfInterest.js";
 import { Utils } from 
     "https://johnmschneider.github.io/projects/rpg%20game%201/utils.js";
 
